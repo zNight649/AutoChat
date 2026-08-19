b68c23dc19fd82e5077c2b402316d0f88b8b8f04
zNight649 <jangutze@gmail.com>
2026-08-19 03:34:16 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("net.labymod.labygradle")
+    id("net.labymod.labygradle.addon")
+}
+
+val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
+
+group = "org.example"
+version = providers.environmentVariable("VERSION").getOrElse("1.0.0")
+
+labyMod {
+    defaultPackageName = "org.example" //change this to your main package name (used by all modules)
+
+    minecraft {
+        registerVersion(versions.toTypedArray()) {
+            runs {
+                getByName("client") {
+                    // When the property is set to true, you can log in with a Minecraft account
+                    // devLogin = true
+                }
+            }
+        }
+    }
+
+    addonInfo {
+        namespace = "example"
+        displayName = "ExampleAddon"
+        author = "Example Author"
+        description = "Example Description"
+        minecraftVersion = "*"
+        version = rootProject.version.toString()
+    }
+}
+
+subprojects {
+    plugins.apply("net.labymod.labygradle")
+    plugins.apply("net.labymod.labygradle.addon")
+
+    group = rootProject.group
+    version = rootProject.version
+
+    extensions.findByType(JavaPluginExtension::class.java)?.apply {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "labymod4-addon-template"
+
+pluginManagement {
+    repositories {
+        maven("https://maven.laby.net/api/v1/maven/release/")
+        maven("https://maven.neoforged.net/releases/")
+        maven("https://maven.fabricmc.net/")
+        gradlePluginPortal()
+        mavenCentral()
+    }
+
+    plugins {
+        id("net.labymod.labygradle.settings") version "0.8.1"
+    }
+}
+
+plugins {
+    id("net.labymod.labygradle.settings")
+}
+
+include(":api")
+include(":core")
